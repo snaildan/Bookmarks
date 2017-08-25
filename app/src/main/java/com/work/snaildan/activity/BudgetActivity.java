c7cc9140d5d65da5c13c175609c11bccc32e0a55
张丹 <zhangdan@gmw.cn>
2017-08-25 13:57:51 +0800

    预算弹出框完成

--- app/src/main/java/com/work/snaildan/activity/BudgetActivity.java
+++ app/src/main/java/com/work/snaildan/activity/BudgetActivity.java
@@ -10,6 +10,9 @@ import android.widget.TextView;
 public class BudgetActivity extends Activity {
     private TextView top_title;
     private ImageView re_top_button;
+    private ImageView budget_set;
+
+    private BudgetAddDialog AddDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +30,19 @@ public class BudgetActivity extends Activity {
                 startActivity(intent);
             }
         });
+        //设置预算弹出自定义对话框
+        budget_set = (ImageView) findViewById(R.id.budget_set);
+        budget_set.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showEditDialog(v);
+            }
+        });
+    }
+
+    //自定义对话框初始化
+    public void showEditDialog(View view) {
+        AddDialog = new BudgetAddDialog(this, R.style.loading_dialog);
+        AddDialog.show();
     }
 }
