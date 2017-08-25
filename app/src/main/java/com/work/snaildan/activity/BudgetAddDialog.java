b3a5670a1291ec763553f223c407d2b1cc635e44
张丹 <zhangdan@gmw.cn>
2017-08-25 15:49:23 +0800

    预算弹出框日期展示

--- app/src/main/java/com/work/snaildan/activity/BudgetAddDialog.java
+++ app/src/main/java/com/work/snaildan/activity/BudgetAddDialog.java
@@ -3,6 +3,7 @@ package com.work.snaildan.activity;
 import android.app.Activity;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.view.Window;
@@ -13,6 +14,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.work.snaildan.db.DbManager;
+import com.work.snaildan.tools.Utools;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BudgetAddDialog extends Dialog {
     final Activity context;
@@ -24,6 +29,9 @@ public class BudgetAddDialog extends Dialog {
     private CheckBox budget_open5;
     private CheckBox budget_open9;
     private CheckBox budget_open100;
+    private List<CheckBox> checkBoxList = new ArrayList<CheckBox>();
+
+    public Utools utools;
 
     //默认皮肤theme不设定即可自行设置
     public BudgetAddDialog(Activity context, int theme) {
@@ -35,8 +43,17 @@ public class BudgetAddDialog extends Dialog {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //指定布局
         setContentView(R.layout.dialog_budget_add);
+
+        budget_open5 = (CheckBox) findViewById(R.id.budget_open5);
+        budget_open9 = (CheckBox) findViewById(R.id.budget_open9);
+        budget_open100 = (CheckBox) findViewById(R.id.budget_open100);
+        budget_editText = (EditText) findViewById(R.id.budget_editText);
+        budget_text = (TextView) findViewById(R.id.budget_text);
+        utools = new Utools();
+        String dateStr = utools.getTimestamp("yyyy-MM");
+        budget_text.setText(dateStr + "预算：");
+
         Window dialogWindow = this.getWindow();
         WindowManager m = context.getWindowManager();
         //获取屏幕宽、高用
@@ -58,5 +75,22 @@ public class BudgetAddDialog extends Dialog {
             }
         });
         this.setCancelable(true);
+        // 为确定按钮绑定点击事件监听器
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (budget_open5.isChecked() == true) {
+                    Log.i("----", "----budget_open5");
+                }
+                if (budget_open9.isChecked() == true) {
+                    Log.i("----", "----budget_open9");
+                }
+                if (budget_open100.isChecked() == true) {
+                    Log.i("----", "----budget_open100");
+                }
+
+
+            }
+        });
     }
 }
