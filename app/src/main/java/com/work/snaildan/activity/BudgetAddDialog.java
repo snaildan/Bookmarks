f89fee9836c656cff07fc8c264165c10e8f13d45
张丹 <zhangdan@gmw.cn>
2017-09-05 16:44:09 +0800

    1.预算提醒完成

--- app/src/main/java/com/work/snaildan/activity/BudgetAddDialog.java
+++ app/src/main/java/com/work/snaildan/activity/BudgetAddDialog.java
@@ -2,7 +2,14 @@ package com.work.snaildan.activity;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.view.Window;
@@ -40,6 +47,16 @@ public class BudgetAddDialog extends Dialog {
     public int WarnFlag = 0;
     private BudgetAdp budgetAdp;
 
+    private int budgetstat = 0;
+    private SharedPreferences sp;
+    private NotificationManager myNotifiManager;
+    public float getLevel1;
+    public float getLevel2;
+    public float getLevel3;
+    public boolean getLevel = false;
+    String msg = "";
+    float moneyVal;
+
     //默认皮肤theme不设定即可自行设置
     public BudgetAddDialog(Activity context, int theme) {
         super(context, theme);
@@ -64,6 +81,11 @@ public class BudgetAddDialog extends Dialog {
         String dateStr = utools.getTimestamp("yyyy-MM");
         budget_text.setText(dateStr + "预算：");
 
+        //获取当月支出总额
+        moneyVal = 0;
+        String mVAl = dbManage.monthTotal("0");
+        moneyVal = Float.parseFloat(mVAl);
+
         //查出预算信息
         String dateStr_ = utools.getTimestamp("yyyy-MM-dd");
         long BudgetDateLong = utools.StringToU(dateStr_, "yyyy-MM");
@@ -72,16 +94,19 @@ public class BudgetAddDialog extends Dialog {
             Float value = tableBudgetOne.get(0).getBudgetMoney();
             String vStr = value + "";
             budget_editText.setText(vStr);
-            Float level1 = tableBudgetOne.get(0).getLevel1();
-            if (level1 > 0) {
+            getLevel1 = tableBudgetOne.get(0).getLevel1();
+            if (getLevel1 > 0) {
+                getLevel = true;
                 budget_open5.setChecked(true);
             }
-            Float level2 = tableBudgetOne.get(0).getLevel2();
-            if (level2 > 0) {
+            getLevel2 = tableBudgetOne.get(0).getLevel2();
+            if (getLevel2 > 0) {
+                getLevel = true;
                 budget_open9.setChecked(true);
             }
-            Float level3 = tableBudgetOne.get(0).getLevel3();
-            if (level3 > 0) {
+            getLevel3 = tableBudgetOne.get(0).getLevel3();
+            if (getLevel3 > 0) {
+                getLevel = true;
                 budget_open100.setChecked(true);
             }
         }
@@ -127,18 +152,21 @@ public class BudgetAddDialog extends Dialog {
                 }
                 if (budget_open5.isChecked() == true) {
                     Level1 = BudgetMoney / 2;
+                    getLevel1 = Level1;
                     WarnFlag = 1;
                 } else {
                     Level1 = 0f;
                 }
                 if (budget_open9.isChecked() == true) {
                     Level2 = BudgetMoney * 0.9f;
+                    getLevel2 = Level2;
                     WarnFlag = 1;
                 } else {
                     Level2 = 0f;
                 }
                 if (budget_open100.isChecked() == true) {
                     Level3 = BudgetMoney;
+                    getLevel3 = Level3;
                     WarnFlag = 1;
                 } else {
                     Level3 = 0f;
@@ -153,10 +181,46 @@ public class BudgetAddDialog extends Dialog {
                 } else {
                     dbManage.updateBudget(BudgetMoney, Level1, Level2, Level3, WarnFlag, BudgetDateLong);
                 }
-                dbManage.closeDB();
                 //budgetAdp传入并将新项目add到列表中
                 budgetAdp.addList(tablebudget);
+
+                sp = context.getSharedPreferences("sp_budget", Context.MODE_PRIVATE);
+                budgetstat = sp.getInt("budgetStat", 0);
+                Log.i("------", "----预警状态：" + budgetstat);
+                Log.i("-----", "--支出：" + moneyVal);
+                Log.i("-----", "--预警1：" + getLevel1);
+                Log.i("-----", "--预警2：" + getLevel2);
+                Log.i("-----", "--预警3：" + getLevel3);
+                if (getLevel && budgetstat == 1) {
+                    //获取预警值和支出值进行预警判断
+                    if ((getLevel3 > 0) && (moneyVal >= getLevel3)) {
+                        msg = "当前支出额度已达到预警值100%";
+                    } else if ((getLevel2 > 0) && (moneyVal >= getLevel2)) {
+                        msg = "当前支出额度已达到预警值90%";
+                    } else if ((getLevel1 > 0) && (moneyVal >= getLevel1)) {
+                        msg = "当前支出额度已达到预警值50%";
+                    } else {
+                        getLevel = false;
+                    }
+                    if (getLevel) {
+                        //准备通知信息
+                        Intent mainIntent = new Intent(context, BudgetActivity.class);
+                        PendingIntent mainPendingIntent = PendingIntent.getActivity(context, 0, mainIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                        myNotifiManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                        Notification notification = new Notification.Builder(context)
+                                .setSmallIcon(R.drawable.ic_launcher)
+                                .setAutoCancel(true)
+                                .setContentTitle("大师理财支出预警")
+                                .setContentText(msg)
+                                .setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE)
+                                .setContentIntent(mainPendingIntent)
+                                .build();
+                        myNotifiManager.notify(1, notification);
+                    }
+                }
                 dismiss();
+                //dbManage.closeDB();
             }
         });
     }
